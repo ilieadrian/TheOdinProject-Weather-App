aacf8d81fcfbad1f7d9b3c6ad2272724c48b7498
I A <ilie.adrian1986@gmail.com>
2024-11-06 10:27:46 +0200

    conttected to the API, extract all the data with variable for city name

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+const city = "Pascani";
+const key = "";
+
+function fetchData() {
+  const link = "";
+        
+    console.log("Getting data")
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city || bucharest}?key=K2LFQUZTBEFMY6X55NN7KWSTF`, {mode: 'cors'})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      console.log(response);
+    });
+    
+}
+
+fetchData()
